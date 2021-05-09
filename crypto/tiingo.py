90e7742c10181a9466e104183a76fc36ec9409c8
Suvranil Ghosh <sg1239@scarletmail.rutgers.edu>
2021-05-08 23:14:36 -0400

    forecasting tests1

--- crypto/tiingo.py
+++ crypto/tiingo.py
@@ -1,4 +1,7 @@
 import requests
+import plotly.graph_objects as go
+import pandas as pd
+from collections import defaultdict
 
 headers = {
     'Content-Type': 'application/json',
@@ -20,3 +23,30 @@ def get_price_data(ticker):
     # print(response.json()[0]['priceData'][0])
     priceData = response.json()[0]['priceData'][0]
     return priceData
+
+def get_historical_price_data(ticker):
+    return None
+
+if __name__ == '__main__':
+    url = "https://api.tiingo.com/tiingo/crypto/prices?tickers=btcusd&startDate=2019-04-29&resampleFreq=1440min"
+    response = requests.get(url, headers = headers)
+    tickerPrice = defaultdict(list)  
+    priceData = response.json()[0]['priceData']
+
+    for prices in priceData:
+        tickerPrice['open'].append(prices['open'])
+        tickerPrice['close'].append(prices['close'])
+        tickerPrice['high'].append(prices['high'])
+        tickerPrice['low'].append(prices['low'])
+        tickerPrice['date'].append(prices['date'].split('T')[0])
+
+    # print(tickerPrice)
+    # priceData = response.json()
+    # print(priceData)
+    fig  = go.Figure(data = [go.Candlestick(x=tickerPrice['date'],
+                                            open=tickerPrice['open'],
+                                            high=tickerPrice['high'],
+                                            low=tickerPrice['low'],
+                                            close=tickerPrice['close'],
+                                            color = 'cyan')])
+    fig.show()
